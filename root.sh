591e2714cebf9ac711f07d50630d52a350d35bf6
MR.X <107016185+cyberytti@users.noreply.github.com>
2022-09-10 21:45:49 +0530

    Add files via upload

---
+++ root.sh
@@ -0,0 +1,21 @@
+echo "process start......"i
+apt update -y
+apt upgrade -y
+pkg install git -y 
+pkg install proot-distro -y
+proot-distro install debian 
+cd
+cd TRoot
+mkdir tt
+mv root /data/data/com.termux/files/home/TRoot/tt
+cd 
+cd TRoot
+cd tt
+chmod +x root
+mv root /data/data/com.termux/files/usr/bin
+cd 
+cd TRoot
+rm -rf tt
+clear
+echo 'Type "root"'
+
